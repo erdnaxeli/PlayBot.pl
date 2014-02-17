a6225ef20fda38fc62fb749cd01d4c0703e4730a
Alexandre Morignot <erdnaxeli@gmail.com>
2014-02-17 17:21:54 +0100

    remove utf8 decoding (fuck that shit)

--- lib/sites/youtube.pm
+++ lib/sites/youtube.pm
@@ -16,11 +16,13 @@ sub get {
     my $video = $yt->get_video_by_id($id);
 
     my %infos;
-    $infos{'title'} = decode("Detect", $video->title);
+    #$infos{'title'} = decode("UTF-8", $video->title);
+    $infos{'title'} = $video->title;
     $infos{'author'} = $video->uploader;
 	$infos{'url'} = $video->base_uri;
 
-    my $context = decode("Detect", $video->description) . ' ' . $infos{'title'};
+=cut
+    my $context = decode("UTF-8", $video->description) . ' ' . $infos{'title'};
     my $finder = URI::Find->new( sub { '' } );
 
     # we remove the URI and the punctuation
@@ -28,6 +30,7 @@ sub get {
     $context =~ s/[[:punct:]]/ /g;
 
     $infos{'context'} = $context;
+=cut
 
 	return %infos;
 }
