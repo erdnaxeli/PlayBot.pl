eb27ac3008200581b1d6e6b576e80dd829d86062
Alexandre Morignot <erdnaxeli@gmail.com>
2014-08-16 23:45:59 +0200

    extend !get -1 to !get -X

--- lib/commands/later.pm
+++ lib/commands/later.pm
@@ -13,7 +13,7 @@ sub exec {
 	$time = 6 if (!$time);
 	$time *= ($unit eq 's') ? 1 : ($unit eq 'm') ? 60 : 3600;
 
-    if ($offset == -1) {
+    while ($offset <= 0) {
         my $sth = $dbh->prepare_cached('
             SELECT content
             FROM playbot_chan
@@ -37,6 +37,7 @@ sub exec {
         my $content = $sth->fetch;
         return unless ($content);
         $id = $content->[0];
+        $offset++;
     }
 
     my $sth = $dbh->prepare_cached('INSERT INTO playbot_later (content, nick, date) VALUES (?, ?, ?)');
