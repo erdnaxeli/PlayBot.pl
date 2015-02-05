e9a2521ed5e998eea11d38459f2fca90f99ffbfc
Alexandre Morignot <erdnaxeli@cervoi.se>
2015-02-05 17:36:37 +0100

    new method utils::db::main_session() and NO AUTO COMMIT

--- lib/utils/db.pm
+++ lib/utils/db.pm
@@ -8,7 +8,7 @@ use JSON;
 use FindBin;
 
 my $conf;
-
+my $dbh;
 
 BEGIN {
     chdir "$FindBin::Bin/";
@@ -18,11 +18,15 @@ BEGIN {
     $conf = decode_json($json);
 }
 
+sub main_session {
+    $dbh = get_new_session() if not $dbh;
+    return $dbh;
+}
 
 sub get_new_session {
     my $dbh = DBI->connect('DBI:mysql:'.$conf->{'bdd'}.';host='.$conf->{'host'}, $conf->{'user'}, $conf->{'passwd'}, {
     	    PrintError => 0,
-	        AutoCommit => 1,
+	        AutoCommit => 0,
     		mysql_auto_reconnect => 1
     	})
     	or die("Couldn't connect to database: ".DBI->errstr);
